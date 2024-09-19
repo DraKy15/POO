f9da7ba302958e86bb7f97e795c4ed17a5183f49
Amet Perez <ametitoop52@gmail.com>
2024-09-18 22:24:40 -0600

    Se ha agregado el menu del hospital en el metodo Main

---
+++ Main.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("\n MENU \n");
+        System.out.println("1. Registrar Paciente");
+        System.out.println("2. Registrar Medico");
+        System.out.println("3. Registrar Consultorio");
+        System.out.println("4. Registrar Consulta");
+
+        int opcion = read.nextInt();
+
+        switch (opcion) {
+
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+
+        }
+
+    }
+
+}
